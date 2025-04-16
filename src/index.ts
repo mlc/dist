be8912284e65258e0b1bce895d8766324d3f78f6
mike castleman <m@mlcastle.net>
2025-04-15 21:40:54 -0400

    support line TPG

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import { Coord, featureCollection, point } from '@turf/helpers';
-import type { Feature, FeatureCollection, Point } from 'geojson';
+import { Coord, featureCollection, lineString, point } from '@turf/helpers';
+import type { Feature, FeatureCollection, LineString, Point } from 'geojson';
 import { writeFile } from 'fs/promises';
 import { getCoord } from '@turf/invariant';
 import { featureEach } from '@turf/meta';
 import { format } from 'prettier';
 import getData from './getData';
 import { decodeCoord, distance as getDist, formatCoord } from './util';
+import pointToLineDistance from '@turf/point-to-line-distance';
 
 export interface Props {
   url?: string;
@@ -16,14 +17,25 @@ export interface Props {
 export type Data = FeatureCollection<Point, Props>;
 
 const decoratePoints = <P extends object>(
-  targetPoint: Coord,
+  targetPoints: Coord[],
   points: FeatureCollection<Point, P>
 ): FeatureCollection<Point, P & { distance: number; strCoord: string }> => {
-  const targetCoord = getCoord(targetPoint) as [number, number];
+  const target: [number, number] | Feature<LineString> =
+    targetPoints.length === 1
+      ? (getCoord(targetPoints[0]) as [number, number])
+      : lineString(targetPoints.map(getCoord));
   const result: Feature<Point, P & { distance: number; strCoord: string }>[] =
     [];
   featureEach(points, (pt) => {
-    const distance = getDist(targetCoord, pt);
+    let distance: number;
+    if (Array.isArray(target)) {
+      distance = getDist(target, pt);
+    } else {
+      distance = pointToLineDistance(pt, target, {
+        units: 'kilometers',
+        method: 'geodesic',
+      });
+    }
     result.push(
       point(getCoord(pt), {
         ...pt.properties,
@@ -37,18 +49,18 @@ const decoratePoints = <P extends object>(
 
 const main = async () => {
   const argc = process.argv.length;
-  if (argc < 3 || argc > 4) {
+  if (argc < 3) {
     throw new Error('provide coords pls');
   }
   const p = await getData();
-  const target = decodeCoord(process.argv[2]);
-  const points = decoratePoints(target, p).features.sort(
+  const targets = process.argv.slice(2).map(decodeCoord);
+  const points = decoratePoints(targets, p).features.sort(
     ({ properties: { distance: a } }, { properties: { distance: b } }) => a - b
   );
   await format(JSON.stringify(points), { parser: 'json' }).then((txt) =>
     writeFile('sorted.json', txt)
   );
-  return argc === 4 ? points.slice(0, parseInt(process.argv[3])) : points[0];
+  return points[0];
 };
 
 main().then((np) => console.dir(np, { depth: null }), console.error);
