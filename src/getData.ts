ebc3b91cef750b9f06d5462eb1e322f248d27892
mike castleman <m@mlcastle.net>
2025-07-11 12:29:19 -0400

    include countries in json

--- src/getData.ts
+++ src/getData.ts
@@ -1,9 +1,14 @@
-import { featureCollection, point } from '@turf/helpers';
+import { Coord, featureCollection, point } from '@turf/helpers';
 import { DOMParser } from '@xmldom/xmldom';
 import { select } from 'xpath';
 import { readFile, stat, writeFile } from 'node:fs/promises';
 import { join as joinPath } from 'node:path';
 import type { Data } from './index';
+import { FeatureCollection, MultiPolygon, Polygon } from 'geojson';
+import Flatbush from 'flatbush';
+import { bbox } from '@turf/bbox';
+import { getCoord } from '@turf/invariant';
+import booleanPointInPolygon from '@turf/boolean-point-in-polygon';
 
 const selector =
   '//all_media/media[geodata/@latitude and string-length(geodata/@latitude)!=0]';
@@ -22,8 +27,50 @@ const read = async (): Promise<Data> => {
   return JSON.parse(data) as Data;
 };
 
+interface CountryProperties {
+  featurecla: 'Admin-0 country';
+  ISO_A2: string;
+  ISO_A3: string;
+  NAME: string;
+  NAME_LONG: string;
+}
+
+type Countries = FeatureCollection<Polygon | MultiPolygon, CountryProperties>;
+
+interface CountryData {
+  index: Flatbush;
+  countries: Countries;
+}
+
+const readCountries = async (): Promise<CountryData> => {
+  const data = await readFile(
+    joinPath(__dirname, '..', 'countries.geojson'),
+    'utf-8'
+  );
+  const countries = JSON.parse(data) as Countries;
+  const index = new Flatbush(countries.features.length);
+  for (const feat of countries.features) {
+    const [minX, minY, maxX, maxY] = bbox(feat);
+    index.add(minX, minY, maxX, maxY);
+  }
+  index.finish();
+  return { index, countries };
+};
+
+const findCountry = (p: Coord, c: CountryData) => {
+  const [x, y] = getCoord(p);
+  const candidates = c.index
+    .search(x, y, x, y)
+    .map((i) => c.countries.features[i]);
+  return candidates.find((candidate) => booleanPointInPolygon(p, candidate))
+    ?.properties?.NAME;
+};
+
 const parse = async (): Promise<Data> => {
-  const xml = await readFile(xmlFilename, 'utf-8');
+  const [xml, countries] = await Promise.all([
+    readFile(xmlFilename, 'utf-8'),
+    readCountries(),
+  ]);
 
   const doc = new DOMParser().parseFromString(xml, 'application/xml');
 
@@ -41,7 +88,16 @@ const parse = async (): Promise<Data> => {
         true
       ) as Attr | undefined;
       const privacy = select('./privacy', node, true) as Element | undefined;
-      return point([Number(lng.value), Number(lat.value)], {
+      const altitude = select(
+        './/exif[@tag="GPSAltitude"]/@raw',
+        node,
+        true
+      ) as Attr | undefined;
+      const altitudeN = altitude
+        ? Number.parseFloat(altitude.value)
+        : undefined;
+      const coords = [Number(lng.value), Number(lat.value)];
+      return point(coords, {
         url: url?.firstChild?.toString(),
         title: title?.firstChild?.toString(),
         imgUrl: imgUrl?.value,
@@ -49,6 +105,11 @@ const parse = async (): Promise<Data> => {
           privacy?.attributes?.getNamedItem('public')?.value === '1'
             ? undefined
             : true,
+        altitude:
+          typeof altitudeN === 'number' && !Number.isNaN(altitudeN)
+            ? altitudeN
+            : undefined,
+        country: findCountry(coords, countries),
       });
     })
   );
